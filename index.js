4e35bbf24c6bdf978e9c65175b94f84a21504179
HandsomeWalker <dreamcreativehub@gmail.com>
2019-06-17 15:03:46 +0800

    some improvement

--- index.js
+++ index.js
@@ -5,14 +5,24 @@
  */
 function handsome(obj, except_res) {
 	let handler = {
-			get: (target, key) => {
-					let val = target()[key] || except_res || '';
-					return new Proxy(() => val, handler);
-			},
-			apply: (target) => {
-					return target();
+		_flag: false,
+		get: (target, key) => {
+			let val
+			if (target() == undefined) {
+				handler._flag = true
+			}else {
+				val = target()[key]
 			}
+			return new Proxy(() => val, handler)
+		},
+		apply: (target) => {
+			let res = target()
+			if (handler._flag) {
+				res = except_res
+			}
+			return res
+		}
 	};
 	return new Proxy(() => obj, handler)
 }
-module.exports = handsome;
+module.exports = handsome
